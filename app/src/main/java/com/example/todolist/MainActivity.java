d7d0d3ca275a4c044eafb3b0812014b8647a413f
menabaddo <edmundalexfresh@gmail.com>
2023-02-01 13:06:21 +0100

    Working on the backend

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.todolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    List<String> toDoList;
+    ArrayAdapter<String>arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toDoList = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<>(this,);
     }
 }
